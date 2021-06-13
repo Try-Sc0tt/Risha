054870ec23b4bccc0a920459305354b05d38040c
Try-Sc0tt <85616246+Try-Sc0tt@users.noreply.github.com>
2021-06-12 20:00:31 -0400

    Add files via upload
    setup love, 
    setup tiled for mapmaker
    made first map and loaded and set up map physics collider layer 

---
+++ Lua Projects/main.lua
@@ -0,0 +1,22 @@
+local STI = require("sti")
+
+function love.load()
+    Map = STI("Map/1.lua", {"box2d"})
+    World = love.physics.newWorld(0,0)
+    Map:box2d_init(World)
+    Map.layers.solid.visible = false
+    background = love.graphics.newImage("assets/backgrounds/background.png")
+end
+
+function love.update(dt)
+    World:update(dt)
+end
+
+
+function love.draw()
+    love.graphics.draw(background)
+    Map:draw(0,0,2,2)
+    love.graphics.push()
+    love.graphics.scale(2,2)
+    love.graphics.pop()
+end
